78f2b55407685e186b76f0716dc31a184e9334ff
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-11-14 08:37:22 -0800

    Asteroid Changes

--- src/base-mixins/password-login.js
+++ src/base-mixins/password-login.js
@@ -1,33 +1,33 @@
 /*
-*   The password-login mixin:
-*   - defines the `createUser` and `loginWithPassword` methods, porcelain for
-*     calling the `createUser` and `login` ddp methods
-*/
+ *   The password-login mixin:
+ *   - defines the `createUser` and `loginWithPassword` methods, porcelain for
+ *     calling the `createUser` and `login` ddp methods
+ */
 
-import {onLogin} from "../common/login-method";
+import { onLogin } from "../common/login-method";
 
 /*
-*   Public methods
-*/
+ *   Public methods
+ */
 
-export function createUser ({username, email, password}) {
-    const options = {
-        password,
-        user: {
-            username,
-            email
-        }
-    };
-    return this.call("createUser", options).then(onLogin.bind(this));
+export function createUser({ username, email, password, profile, ...rest }) {
+  const options = {
+    password,
+    username,
+    email,
+    profile,
+    ...rest,
+  };
+  return this.call("createUser", options).then(onLogin.bind(this));
 }
 
-export function loginWithPassword ({username, email, password}) {
-    const loginParameters = {
-        password,
-        user: {
-            username,
-            email
-        }
-    };
-    return this.call("login", loginParameters).then(onLogin.bind(this));
+export function loginWithPassword({ username, email, password }) {
+  const loginParameters = {
+    password,
+    user: {
+      username,
+      email
+    }
+  };
+  return this.call("login", loginParameters).then(onLogin.bind(this));
 }
--- src/common/login-method.js
+++ src/common/login-method.js
@@ -1,29 +1,29 @@
 import * as multiStorage from "./multi-storage";
 
-export function onLogin ({id, token}) {
-    this.userId = id;
-    this.loggedIn = true;
-    return multiStorage.set(this.endpoint + "__login_token__", token)
-        .then(this.emit.bind(this, "loggedIn", id))
-        .then(() => id);
+export function onLogin({ id, token }) {
+  this.userId = id;
+  this.loggedIn = true;
+  return multiStorage.set('Meteor.loginToken', token)
+    .then(this.emit.bind(this, "loggedIn", id))
+    .then(() => id);
 }
 
-export function onLogout () {
-    this.userId = null;
-    this.loggedIn = false;
-    return multiStorage.del(this.endpoint + "__login_token__")
-        .then(this.emit.bind(this, "loggedOut"))
-        .then(() => null);
+export function onLogout() {
+  this.userId = null;
+  this.loggedIn = false;
+  return multiStorage.del('Meteor.loginToken')
+    .then(this.emit.bind(this, "loggedOut"))
+    .then(() => null);
 }
 
-export function resumeLogin () {
-    return multiStorage.get(this.endpoint + "__login_token__")
-        .then(resume => {
-            if (!resume) {
-                throw new Error("No login token");
-            }
-            return {resume};
-        })
-        .then(this.login.bind(this))
-        .catch(onLogout.bind(this));
+export function resumeLogin() {
+  return multiStorage.get('Meteor.loginToken')
+    .then(resume => {
+      if (!resume) {
+        throw new Error("No login token");
+      }
+      return { resume };
+    })
+    .then(this.login.bind(this))
+    .catch(onLogout.bind(this));
 }
